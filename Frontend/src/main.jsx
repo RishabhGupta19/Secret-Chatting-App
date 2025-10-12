105d011b125e6eb1d31d9b21f13354ae905db146
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-12 18:49:43 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,40 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import App from "./App.jsx"; // This will be your Host UI
+// import Guest from "./Guest.jsx"; // This is your Guest UI
+// import Chatting from './chatting'
+// // Define routes
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/guestroom" element={<Guest />} />
+//         <Route path="/createchat" element={<Chatting />} />
+//       </Routes>
+//     </Router>
+//   </StrictMode>
+// );
+
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>
+// );
+
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx"; // This will be your Host UI
-import Guest from "./Guest.jsx"; // This is your Guest UI
-import Chatting from './chatting'
-// Define routes
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Guest from "./Guest.jsx";
+import Chatting from "./chatting";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -20,10 +48,4 @@ createRoot(document.getElementById("root")).render(
   </StrictMode>
 );
 
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
 
