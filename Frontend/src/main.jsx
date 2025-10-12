f28afbd8f29521165779e5a99e978f32166ca17b
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-12 18:44:09 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,20 +6,20 @@ import App from "./App.jsx"; // This will be your Host UI
 import Guest from "./Guest.jsx"; // This is your Guest UI
 import Chatting from './chatting'
 // Define routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />, // Host route
-  },
-  {
-    path: "/guestroom",
-    element: <Guest />, // Guest route
-  },
-  {
-    path:"/createchat",
-    element:<Chatting/>
-  }
-]);
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/guestroom" element={<Guest />} />
+        <Route path="/createchat" element={<Chatting />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
