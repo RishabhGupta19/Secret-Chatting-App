d340d8708afd7c7b62722fbdca82fa9915b34737
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-12 15:54:23 +0530

    Update socket.js

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://secret-chatting-app.onrender.com", {
   transports: ["websocket"], // ensures stable connection
 });
