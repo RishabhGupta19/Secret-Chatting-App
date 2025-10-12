22b28e10afe810c4c1a28767cb762256e2e1c061
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-10-12 18:55:09 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -28,7 +28,7 @@
 // );
 
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -47,5 +47,3 @@ createRoot(document.getElementById("root")).render(
     </Router>
   </StrictMode>
 );
-
-
