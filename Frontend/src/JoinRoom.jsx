5e568856ef2ca65dc20505fa0a43e59a40b77076
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-11-01 16:47:06 +0530

    Update JoinRoom.jsx

--- Frontend/src/JoinRoom.jsx
+++ Frontend/src/JoinRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { io } from "socket.io-client";
-const socket = io("http://43.204.112.122:5000");
+const socket = io("https://secret-chatting-app.onrender.com");
 
 function JoinRoom({ onJoinSuccess }) {
   const [code, setCode] = useState("");
